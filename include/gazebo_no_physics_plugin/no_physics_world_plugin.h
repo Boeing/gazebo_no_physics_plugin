db67dbf87ad88bbd989434070b8dc51007a94399
leandroruiz <leandro.ruiz@mtorres.com>
2022-11-11 05:59:54 -0800

    migrated plugin to ROS2 from Boeing repo

---
+++ include/gazebo_no_physics_plugin/no_physics_world_plugin.h
@@ -0,0 +1,27 @@
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/physics/World.hh>
+#include <gazebo/common/Events.hh>
+
+namespace gazebo_plugins
+{
+
+    class GazeboNoPhysicsPlugin : public gazebo::WorldPlugin
+    {
+    private:
+        gazebo::physics::WorldPtr world_;
+        gazebo::event::ConnectionPtr update_connection_;
+
+    public:
+        GazeboNoPhysicsPlugin();
+        virtual ~GazeboNoPhysicsPlugin();
+
+    protected:
+        void Load(gazebo::physics::WorldPtr world, sdf::ElementPtr sdf);
+
+    private:
+        void onWorldUpdateBegin();
+    };
+
+    GZ_REGISTER_WORLD_PLUGIN(GazeboNoPhysicsPlugin)
+
+} // namespace gazebo
---
+++ src/no_physics_world_plugin.cpp
@@ -0,0 +1,31 @@
+#include <gazebo_no_physics_plugin/no_physics_world_plugin.h>
+
+namespace gazebo_plugins
+{
+
+    GazeboNoPhysicsPlugin::GazeboNoPhysicsPlugin() {}
+    GazeboNoPhysicsPlugin::~GazeboNoPhysicsPlugin() {}
+
+    void GazeboNoPhysicsPlugin::Load(gazebo::physics::WorldPtr world, sdf::ElementPtr sdf)
+    {
+        printf("Gazebo Plugin: 'No World Physics' loaded...");
+
+        world_ = world;
+        update_connection_ =
+            gazebo::event::Events::ConnectWorldUpdateBegin(boost::bind(&GazeboNoPhysicsPlugin::onWorldUpdateBegin, this));
+    }
+
+    void GazeboNoPhysicsPlugin::onWorldUpdateBegin()
+    {
+#if GAZEBO_MAJOR_VERSION >= 8
+        if (world_->PhysicsEnabled())
+        {
+            world_->SetPhysicsEnabled(false);
+        }
+#else
+        if (world_->GetEnablePhysicsEngine())
+            world_->EnablePhysicsEngine(false);
+#endif
+    }
+
+} // namespace gazebo
