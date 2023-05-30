a79d46366538bf95923d5b5b9ca8ed4f0f5cfc9e
Leandro Ruiz <leandro.ruiz@mtorres.com>
2023-05-30 09:27:51 +0200

    clang

--- include/gazebo_no_physics_plugin/no_physics_world_plugin.h
+++ include/gazebo_no_physics_plugin/no_physics_world_plugin.h
@@ -2,24 +2,26 @@
 #include <gazebo/common/Plugin.hh>
 #include <gazebo/physics/World.hh>
 
-namespace gazebo_plugins {
+namespace gazebo_plugins
+{
 
-class GazeboNoPhysicsPlugin : public gazebo::WorldPlugin {
-private:
-  gazebo::physics::WorldPtr world_;
-  gazebo::event::ConnectionPtr update_connection_;
+class GazeboNoPhysicsPlugin : public gazebo::WorldPlugin
+{
+  private:
+    gazebo::physics::WorldPtr world_;
+    gazebo::event::ConnectionPtr update_connection_;
 
-public:
-  GazeboNoPhysicsPlugin();
-  virtual ~GazeboNoPhysicsPlugin();
+  public:
+    GazeboNoPhysicsPlugin();
+    virtual ~GazeboNoPhysicsPlugin();
 
-protected:
-  void Load(gazebo::physics::WorldPtr world, sdf::ElementPtr sdf);
+  protected:
+    void Load(gazebo::physics::WorldPtr world, sdf::ElementPtr sdf);
 
-private:
-  void onWorldUpdateBegin();
+  private:
+    void onWorldUpdateBegin();
 };
 
 GZ_REGISTER_WORLD_PLUGIN(GazeboNoPhysicsPlugin)
 
-} // namespace gazebo_plugins
+}  // namespace gazebo_plugins
